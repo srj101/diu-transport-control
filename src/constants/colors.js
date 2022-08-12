66f3f87cf60f028ae8a7bcf7dd134f78e78aebc5
SheikhFoysaldiu <foysal15-3442@diu.edu.bd>
2022-08-12 06:06:54 -0700

    Testing

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,10 +1,10 @@
 const colors = {
-  green: "#4CAF50",
-  red: "#DB190C",
-  purple: "#8624DB",
-  text: "#fff",
-  orange: "#FF9066",
-  accent: "#616161",
+  green: '#4CAF50',
+  red: '#DB190C',
+  purple: '#8624DB',
+  text: '#fff',
+  orange: '#FF9066',
+  accent: '#616161',
 };
-
+//comments
 export default colors;
